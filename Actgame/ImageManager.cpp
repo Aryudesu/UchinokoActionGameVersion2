b1882bb13179c9491b652b162bc1fdc45d3b6832
Aryu <suwako_a-u-@hotmail.co.jp>
2026-08-20 00:18:34 +0900

    画像未読込時の範囲外アクセスを防止

--- Actgame/ImageManager.cpp
+++ Actgame/ImageManager.cpp
@@ -6,6 +6,12 @@
 //コンストラクタ
 ImageManager::ImageManager() { img.resize(32); }
 
+bool ImageManager::IsValidImage(int ID, int num) const {
+	return ID >= 0 && ID < static_cast<int>(img.size()) &&
+		num >= 0 && num < static_cast<int>(img[ID].size()) &&
+		img[ID][num] >= 0;
+}
+
 //透過色設定
 void ImageManager::SetTrans(int R, int G, int B) { SetTransColor(R, G, B); }
 
@@ -41,6 +47,12 @@ void ImageManager::LoadImg(int ID, const std::string& FileName) {
 
 //画像サイズ
 void ImageManager::GetSize(int ID, int num, int &width, int &height) {
+	if (!IsValidImage(ID, num)) {
+		// Callers use the size for division/modulo when drawing backgrounds.
+		width = 1;
+		height = 1;
+		return;
+	}
 	int x, y;
 	GetGraphSize(img[ID][num],&x,&y);
 	width  = x;
@@ -49,6 +61,11 @@ void ImageManager::GetSize(int ID, int num, int &width, int &height) {
 
 //画像サイズ
 void ImageManager::GetSize(int ID,int &width, int &height) {
+	if (!IsValidImage(ID, 0)) {
+		width = 1;
+		height = 1;
+		return;
+	}
 	int x, y;
 	GetGraphSize(img[ID][0], &x, &y);
 	width = x;
@@ -58,6 +75,7 @@ void ImageManager::GetSize(int ID,int &width, int &height) {
 
 //IDのオブジェクトの画像破棄
 void ImageManager::DestroyImg(int ID) {
+	if (ID < 0 || ID >= static_cast<int>(img.size())) return;
 	for (const int handle : img[ID]) {
 		DeleteGraph(handle);
 	}
@@ -68,18 +86,27 @@ void ImageManager::DestroyImg(int ID) {
 //x,y　画面位置
 //ID,num　オブジェクトIDと描画番号
 //TransFlag　透過するかしないか　TRUE/FALSE
-void ImageManager::DrawImg(float x, float y, int ID, int num, int TransFlag,int TurnY) { DrawRotaGraph2(x, y,0,0, 1, 0, img[ID][num], TransFlag, FALSE, TurnY); }
+void ImageManager::DrawImg(float x, float y, int ID, int num, int TransFlag,int TurnY) {
+	if (!IsValidImage(ID, num)) return;
+	DrawRotaGraph2(x, y,0,0, 1, 0, img[ID][num], TransFlag, FALSE, TurnY);
+}
 
 //画像描画（0,0に描画）
 //ID　オブジェクトID
 //TransFlag　透過するかしないか　TRUE/FALSE
-void ImageManager::DrawImg(int ID, int TransFlag) { DrawGraph(0, 0, img[ID][0], TransFlag); }
+void ImageManager::DrawImg(int ID, int TransFlag) {
+	if (!IsValidImage(ID, 0)) return;
+	DrawGraph(0, 0, img[ID][0], TransFlag);
+}
 
 //画像描画
 //x,y　画面位置
 //ID,num　オブジェクトIDと描画番号
 //TransFlag　透過するかしないか　TRUE/FALSE
-void ImageManager::DrawImg(float x, float y, int ID, int TransFlag) { DrawGraph(x, y, img[ID][0], TransFlag); }
+void ImageManager::DrawImg(float x, float y, int ID, int TransFlag) {
+	if (!IsValidImage(ID, 0)) return;
+	DrawGraph(x, y, img[ID][0], TransFlag);
+}
 
 
 void ImageManager::DeleteAll() {
