93d15c7cdd7dc01b305d61c5d9ea215c8db14c83
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-16 01:12:09 +0900

    坂道の垂直面に側面衝突を追加

--- Actgame/Foundation/TerrainCollision.h
+++ Actgame/Foundation/TerrainCollision.h
@@ -14,6 +14,11 @@ struct GroundHit {
 
 class TerrainCollision {
 public:
+	enum class TileSide {
+		Left,
+		Right
+	};
+
 	static bool TryGetSurfaceY(
 		CollisionShape Shape,
 		TilePosition Tile,
@@ -21,6 +26,16 @@ public:
 		int TileWidth,
 		int TileHeight,
 		float& SurfaceY);
+
+	// タイルの左右端に実体がある場合、その垂直区間を返す。
+	static bool TryGetSideBlock(
+		CollisionShape Shape,
+		TilePosition Tile,
+		TileSide Side,
+		int TileWidth,
+		int TileHeight,
+		float& BlockTop,
+		float& BlockBottom);
 
 	static bool FindGround(
 		const TileMap& Map,
