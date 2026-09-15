63a3642bf4ef7e71a17301613bb02632a3f42032
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-16 01:12:07 +0900

    坂道の垂直面に側面衝突を追加

--- Actgame/Foundation/CharacterController.h
+++ Actgame/Foundation/CharacterController.h
@@ -33,6 +33,8 @@ public:
 
 private:
 	bool IsSolid(const TileMap& Map, const TileCatalog& Catalog, int Column, int Row) const;
+	bool IsSideBlocked(const TileMap& Map, const TileCatalog& Catalog,
+		int Column, int Row, bool TargetLeftSide, float MaxStepUp) const;
 	void MoveHorizontal(float Amount, const TileMap& Map, const TileCatalog& Catalog);
 	void MoveVertical(float Amount, const TileMap& Map, const TileCatalog& Catalog);
 	bool FindGroundAtFeet(float FootY, float MaxRise, float MaxDrop,
