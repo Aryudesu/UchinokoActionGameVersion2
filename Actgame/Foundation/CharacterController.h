3b84dc48b7131c41b4bc2b5295fb1a8b7435c76f
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-17 01:37:31 +0900

    天井の継ぎ目に補助判定を追加

--- Actgame/Foundation/CharacterController.h
+++ Actgame/Foundation/CharacterController.h
@@ -33,7 +33,7 @@ public:
 
 private:
 	bool IsCeilingBlocked(const TileMap& Map, const TileCatalog& Catalog,
-		int Column, int Row, WorldPosition Head) const;
+		WorldPosition Head) const;
 	bool IsSideBlocked(const TileMap& Map, const TileCatalog& Catalog,
 		int Column, int Row, bool TargetLeftSide) const;
 	void MoveHorizontal(float Amount, const TileMap& Map, const TileCatalog& Catalog);
