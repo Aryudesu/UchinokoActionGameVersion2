e8025e4d67acfdfe9e7b0cf3e868be10720eb541
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-17 00:59:43 +0900

    坂道の接地判定を足元中央へ統一

--- Actgame/SlopeSandboxScene.cpp
+++ Actgame/SlopeSandboxScene.cpp
@@ -111,6 +111,8 @@ void SlopeSandboxScene::draw() {
 	DrawBox(static_cast<int>(Body.Position.X), static_cast<int>(Body.Position.Y),
 		static_cast<int>(Body.Position.X + Body.Width), static_cast<int>(Body.Position.Y + Body.Height),
 		GetColor(240, 210, 80), TRUE);
+	DrawCircle(static_cast<int>(Body.Position.X + Body.Width * 0.5f),
+		static_cast<int>(Body.Position.Y + Body.Height), 3, GetColor(255, 80, 80), TRUE);
 	DrawString(16, 16, "Slope/Stair test: Left/Right move, Z jump, R reload, Esc menu", GetColor(255, 255, 255));
 	DrawString(16, 40, Body.Grounded ? "Grounded" : "Airborne", GetColor(255, 255, 255));
 }
