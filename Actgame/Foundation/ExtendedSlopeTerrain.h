e146c3b0af6510beed26c8d3b027224334dffdfe
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-19 01:37:02 +0900

    2x1坂の高い端からの落下を修正: Actgame/Foundation/ExtendedSlopeTerrain.h

--- Actgame/Foundation/ExtendedSlopeTerrain.h
+++ Actgame/Foundation/ExtendedSlopeTerrain.h
@@ -19,7 +19,8 @@ public:
 	static bool TryCharacterY(const TileMap& Map, const TileCatalog& Catalog,
 		float WorldX, float ProbeY, float& CharacterY, Slope2x1* Found = nullptr);
 	static bool FollowHorizontal(const TileMap& Map, const TileCatalog& Catalog,
-		float OldX, float NewX, float OldY, float& NewY, bool WasGrounded);
+		float OldX, float NewX, float OldY, float& NewY, bool WasGrounded,
+		bool& Grounded);
 	static bool ResolveHighSide(const TileMap& Map, const TileCatalog& Catalog,
 		float OldX, float& NewX, float Y, bool MovingRight, bool Grounded);
 	static bool ResolveFalling(const TileMap& Map, const TileCatalog& Catalog,
