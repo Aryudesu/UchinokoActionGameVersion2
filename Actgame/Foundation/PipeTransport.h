a2c53ff668033f0e82bf7ef8907203a7f3973ab8
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-21 02:43:54 +0900

    土管フェード時間をさらに約2倍に延ばす

--- Actgame/Foundation/PipeTransport.h
+++ Actgame/Foundation/PipeTransport.h
@@ -32,7 +32,7 @@ enum class PipeTransportPhase {
 class PipeTransport {
 public:
 	static constexpr int TransitionFrames = 32;
-	static constexpr int FadeStep = 16;
+	static constexpr int FadeStep = 8;
 
 	void Reset();
 
