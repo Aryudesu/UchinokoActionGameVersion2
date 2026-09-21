3dc5b4df0ebcf50237abcd1c6ca9d853dba28112
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-21 15:39:48 +0900

    Add native stage data loader interface

---
+++ Actgame/Foundation/NativeStageDataLoader.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Result.h"
+#include "StageData.h"
+
+#include <string>
+
+namespace uchinoko {
+
+class NativeStageDataLoader {
+public:
+	static Result<StageData> Load(const std::string& FileName);
+	static Result<StageData> Parse(
+		const std::string& JsonText,
+		const std::string& BaseDirectory = "");
+};
+
+} // namespace uchinoko
