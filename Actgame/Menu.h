c3b36d75ee74a93a6c96c825b42065bd72c27011
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-21 18:14:17 +0900

    Add native stage sandbox menu entry

--- Actgame/Menu.h
+++ Actgame/Menu.h
@@ -7,12 +7,13 @@
 #define MUSIC_ROOM  1
 #define SLOPE_TEST  2
 #define GIMMICK_TEST 3
+#define NATIVE_STAGE_TEST 4
 
 class Menu :public Scene{
 protected:
 	int Select;
 	int Space = 30;
-	std::vector<std::string> SelectStr = { "開始","サウンドトラック","Slope test","Gimmick test","終了" };
+	std::vector<std::string> SelectStr = { "開始","サウンドトラック","Slope test","Gimmick test","Native stage test","終了" };
 public:
 	Menu();
 	void update();
